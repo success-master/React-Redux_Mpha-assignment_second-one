77a3598db67452709f7cb7aa7dc98c76a01080f9
success-master <joybrother2019@gmail.com>
2021-11-17 14:05:01 -0600

    update reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,4 +1,4 @@
-export default (state, action) => {
+const rootReducer = (state, action) => {
     switch (action.type) {
         case "resultAction":
             return {
@@ -9,3 +9,5 @@ export default (state, action) => {
             return state;
     }
 };
+
+export default rootReducer
